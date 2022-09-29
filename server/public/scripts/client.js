74339fee83e2b80367821909ebaf5a5f6439faa2
Crystal Redmon <cato2848@gmail.com>
2022-09-29 10:16:14 -0500

    setup onsubmitguess

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,24 @@
 $(document).ready(handleReady);
 
 function handleReady() {
-  console.log("jquery is loaded!")
+  console.log("jquery is loaded!");
+
+  $('#form').on('submit', onSubmitGuess);
+
+
+
+}
+
+
+function onSubmitGuess(evt){
+    evt.preventDefault();
+
+    console.log('Test test');
+
+    let newInput ={
+      player1: $('#player1').val(),
+      player2: $('#player2').val() 
+    }
+    console.log(newInput);
+
 }
